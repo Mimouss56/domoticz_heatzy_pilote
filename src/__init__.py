87a68ca88504665c556301c8eff8b70872e7bc80
GitHub Action <action@github.com>
2025-10-25 20:48:49 +0000

    chore: bump version to 1.4.0

--- src/__init__.py
+++ src/__init__.py
@@ -21,7 +21,7 @@ The module is organized as follows:
 - logger: Logging utilities
 """
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 __author__ = "Mimouss56"
 
 # Make main classes available at package level
